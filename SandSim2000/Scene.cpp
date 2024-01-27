6d81f308d416b4d5c8c11cb72df8dd03bc9941e6
Sam <sfmoon47@gmail.com>
2024-01-27 15:35:02 +0000

    Fixed culling issue in Scene due to changes in sprite positioning in Camera. Also fixed paramter issue in findViewportIterators.

--- SandSim2000/Scene.cpp
+++ SandSim2000/Scene.cpp
@@ -10,17 +10,14 @@ void Scene::UpdateGameScene(Camera& cam, GameState& gameState) {
 	sf::IntRect viewbounds(0, 0, cam.window.getSize().x, cam.window.getSize().y);
 
 	gameScene.clear();
-	//this was throwing an error, too few arguments, it was missing the index argument
-	//so i quickly added this.
-	unsigned int index = 0;
-	findViewportIterators(gameState.quadTree, cam, gridGenerator, viewbounds, index);
+	findViewportIterators(gameState.quadTree, cam, gridGenerator, viewbounds);
 }
 
-void Scene::findViewportIterators(QuadTree* root, Camera& cam, GridGenerator& gridGenerator, sf::IntRect& viewbounds, unsigned int& index) {
+void Scene::findViewportIterators(QuadTree* root, Camera& cam, GridGenerator& gridGenerator, sf::IntRect& viewbounds) {
 
 	int screenX, screenY;
 	sf::Vector2f isometricPosition = gridGenerator.cartesianToIsometricTransform(sf::Vector2f(root->quadRect.getPosition().x / TILE_SIZE, root->quadRect.getPosition().y / TILE_SIZE));
-	cam.WorldToScreen(isometricPosition.x + cam.window.getSize().x / 2, isometricPosition.y - (TILE_SIZE / 2) * MAX_TILE_DEPTH, screenX, screenY);
+	cam.WorldToScreen(isometricPosition.x + cam.window.getSize().x / 2, isometricPosition.y, screenX, screenY);
 
 	int sizeX = root->quadRect.getSize().x * cam.scaleX;
 	int sizeY = (root->quadRect.getSize().y / 2 + (TILE_SIZE / 2) * MAX_TILE_DEPTH) * cam.scaleY;
@@ -30,11 +27,10 @@ void Scene::findViewportIterators(QuadTree* root, Camera& cam, GridGenerator& gr
 	if (!viewbounds.intersects(isometricNodeRect))
 		return;
 
-
 	if (typeid(*root) == typeid(QuadTreeLeaf)) {
 		gameScene.insert(((QuadTreeLeaf*)root)->iter);
 	} else {
 		for (QuadTree* child : ((QuadTree*)root)->children)
-			findViewportIterators(child, cam, gridGenerator, viewbounds, index);
+			findViewportIterators(child, cam, gridGenerator, viewbounds);
 	}
 }
