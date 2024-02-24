bb59f8801ec252354849cc13be7565a27e83fb4d
Samuel K D L Davidson <123samueld@gmail.com>
2024-02-24 13:55:49 +0000

    Boilerplate code added for Scenery Task.

---
+++ SandSim2000/SceneryManager.h
@@ -0,0 +1,8 @@
+#pragma once
+class SceneryManager
+{
+	// This will be used to handle placing scenery
+	// To accomplish this it will need to interact with the InputState and InputStateManager
+	// to receive input from the player to place scenery on command. Start with the tree sprite. 
+};
+
