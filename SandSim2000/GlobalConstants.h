490d94d57bc10d461f2b4c06ff23f10432189ee2
Samuel K D L Davidson <123samueld@gmail.com>
2024-06-04 14:37:54 +0100

    Added Global Const

---
+++ SandSim2000/GlobalConstants.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class GlobalConstants {
+public:
+    static const int cellSize = 100;
+    static const int terrainSpiteWidth = 100;
+    static const int terrainSpiteHeight = 100;
+    static const int quadTreeDepth = 5;
+    static const int mapSize = 16;
+    
+};
+
