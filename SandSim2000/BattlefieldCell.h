26d8c3b14a23bb0e1bb0ac3e1149bd5eadb1ca9b
Samuel K D L Davidson <123samueld@gmail.com>
2024-06-07 10:37:53 +0100

    Updated BattlefieldCell.

--- SandSim2000/BattlefieldCell.h
+++ SandSim2000/BattlefieldCell.h
@@ -1,18 +1,30 @@
 #pragma once
 #include "SFML/Graphics.hpp"
+
 #include <vector>
+
 #include "Agent.h"
 
 
 class BattlefieldCell {
 public:
 
-    int x, y; 
+    int x, y;
     int windowX, windowY;
     std::vector<sf::Vector2i> vertices;
- 
+
     std::vector<Agent> Objects = std::vector<Agent>();
     sf::Sprite* terrainSprite;
     int YOffset;
 
+    bool impassableTerrain = false;
+    bool inClosedList = false;
+
+    int gScore = 0;
+    int hScore = 0;
+    int fScore = 0;
+
+    BattlefieldCell* pathParent = nullptr;
+
+    int id;
 };
