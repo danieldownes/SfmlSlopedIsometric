d454cfe624d063e4f69fbcebc954e488ff8c7e77
liam1stewart2@gmail.com <liam1stewart2@gmail.com>
2024-06-15 17:33:50 +0100

    removed comments from TheGreatWar.cpp

--- SandSim2000/TheGreatWar.cpp
+++ SandSim2000/TheGreatWar.cpp
@@ -21,15 +21,6 @@ int main() {
     Camera camera;
     Scene scene;
 
-    // Liam: Instead of setting the sprite to the Pathfinder Agent, please set the Red Baron sprite. 
-    // I'm not fully tracking how you're updating the position of the Pathfinder Agent but 
-    // in the Animation Manager please create functionality to pick the right part of the sprite sheet
-    // depending on which direction the sprite is facing. This will require adding a "direction" property 
-    // to the Agent class, 0-7 for directions. Then make sure the Pathfinder Agent Red Baron sprite
-    // is facing in the right direction as it follows the path. 
-    // Add trees with T -- Liam
-
-
     agentManager.placePathfinderAgent(sf::Vector2i(8, 8), &scene.gameScene, PathfinderAgent(8, 8, "PathfinderAgent"), gameStateManager);
 
     agentManager.placeScenery(sf::Vector2i(7, 8), &scene.gameScene, Tree(7, 8), gameStateManager);
@@ -39,21 +30,15 @@ int main() {
 
     while (camera.window.isOpen()) {
         inputState = InputStateManager::getInstance().updateInputState(camera.window, inputState);
-        /*
+
         sf::Event event;
         while (camera.window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 camera.window.close();
             }
-            else if (event.type == sf::Event::MouseWheelScrolled) {
-                camera.Zoom(event);
-            }
-        }
-        */
 
         if(inputState.isTPressed)
             agentManager.placeScenery(inputState.selectedCell, &scene.gameScene, Tree(inputState.selectedCell.x, inputState.selectedCell.y), gameStateManager);
-
 
         agentManager.onUpdate(inputState, &scene.gameScene, gameStateManager, camera, scene);
 
