14669252f63695238c975a9d98f72e046ac07fc1
Daniel Downes <dd@danieldownes.co.uk>
2024-06-20 23:10:07 +0700

    Fix pathfinding merge issue

--- SandSim2000/TheGreatWar.cpp
+++ SandSim2000/TheGreatWar.cpp
@@ -7,42 +7,34 @@ int main()
     MenuOption selectedOption = displayMenu(window);
     if (selectedOption == MenuOption::Exit)
         return 0;
+    else if (selectedOption == MenuOption::MapEditor)
+        std::cout << "Map Editor selected\n";
 
-    // Initialize Components
     SpriteManager sprites = SpriteManager();
     AnimationManager animationManager;
     unsigned int mapSize = 16;
     GameStateManager gameStateManager = GameStateManager(mapSize * mapSize);
     AgentManager agentManager;
-    InputState inputState = InputState();
+    InputState inputState;
     Camera camera;
     Scene scene;
 
-    if (selectedOption == MenuOption::MapEditor)
-        std::cout << "Map Editor selected\n";
-    
-    else if (selectedOption == MenuOption::Skirmish)
-        agentManager.placePathfinderAgent(sf::Vector2i(8, 8), &scene.gameScene,
-            PathfinderAgent(8, 8, "PathfinderAgent"), gameStateManager);
+    agentManager.placePathfinderAgent(sf::Vector2i(8, 8), &scene.gameScene, PathfinderAgent(8, 8, "PathfinderAgent"), gameStateManager);
 
     while (camera.window.isOpen())
     {
         inputState = InputStateManager::getInstance().updateInputState(camera.window, inputState);
 
-        agentManager.placePathfinderAgent(sf::Vector2i(8, 8), &scene.gameScene, PathfinderAgent(8, 8, "PathfinderAgent"), gameStateManager);
-
-        if(inputState.isTPressed)
+        if (inputState.isTPressed)
             agentManager.placeScenery(inputState.selectedCell, &scene.gameScene, Tree(inputState.selectedCell.x, inputState.selectedCell.y), gameStateManager);
 
         agentManager.onUpdate(inputState, &scene.gameScene, gameStateManager, camera, scene);
 
         if (!camera.Update(inputState))
             break;
-
         scene.UpdateGameScene(camera, gameStateManager.getState(), inputState);
         camera.Draw(scene.buildGameScene(&animationManager), inputState);
-
-        window.display();
     }
-    return 0;
+
+	return 0;
 }
