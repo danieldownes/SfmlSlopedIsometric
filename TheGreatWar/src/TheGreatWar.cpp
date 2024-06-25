367fd07592247112fd109cdfa2cce879cc1bc183
Daniel Downes <dd@danieldownes.co.uk>
2024-06-25 22:46:35 +0700

    Fix merge of TheGreatWar.cpp

--- TheGreatWar/src/TheGreatWar.cpp
+++ TheGreatWar/src/TheGreatWar.cpp
@@ -2,11 +2,16 @@
 
 int main()
 {
+	// Menu is currently not implemented so disable call for now
+	/*
 	MenuOption selectedOption = displayMenu();
 	if (selectedOption == MenuOption::Exit)
 		return 0;
 	else if (selectedOption == MenuOption::MapEditor)
-		std::cout << "Map Editor selected\n";
+		{ }
+	*/
+
+	sf::Clock clock;
 
 	SpriteManager sprites = SpriteManager();
 	AnimationManager animationManager;
@@ -19,8 +24,13 @@ int main()
 
 	agentManager.placePathfinderAgent(sf::Vector2i(8, 8), &scene.gameScene, PathfinderAgent(8, 8, "PathfinderAgent"),
 									  gameStateManager);
-	agentManager.placeMobileAgent(sf::Vector2i(8, 8), &scene.gameScene, MobileAgent(8, 8, 1, 1, 0.1f, 1, "RedBaron"),
+	agentManager.placeMobileAgent(sf::Vector2i(8, 8), &scene.gameScene, MobileAgent(8, 8, 1, 1, 1.7f, 1, "RedBaron"),
 								  gameStateManager);
+
+	// Centre Camera to RedBaron position
+	GridGenerator grid;
+	sf::Vector2f v = grid.transformToIsometric(8, 8);
+	camera.SetOffset(v);
 
 	agentManager.placeScenery(sf::Vector2i(7, 8), &scene.gameScene, Tree(7, 8), gameStateManager);
 	agentManager.placeScenery(sf::Vector2i(9, 8), &scene.gameScene, Tree(9, 8), gameStateManager);
@@ -35,20 +45,23 @@ int main()
 		while (camera.window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
-			{
 				camera.window.close();
-			}
 		}
 		if (inputState.isTPressed)
 			agentManager.placeScenery(inputState.selectedCell, &scene.gameScene,
 									  Tree(inputState.selectedCell.x, inputState.selectedCell.y), gameStateManager);
 
-		agentManager.Update(inputState, &scene.gameScene, gameStateManager, camera, scene);
+		float deltaTime = clock.restart().asSeconds();
 
-		if (!camera.Update(inputState))
-			break;
-		scene.UpdateGameScene(camera, gameStateManager.getState(), inputState);
+		agentManager.Update(deltaTime, inputState, &scene.gameScene, gameStateManager, camera, scene);
+		camera.Update(deltaTime, inputState);
+
+		scene.Update(camera, gameStateManager.getState(), inputState);
+
 		camera.Draw(scene.buildGameScene(&animationManager), inputState);
+
+		if (inputState.isEscapePressed)
+			break;
 	}
 
 	return 0;
